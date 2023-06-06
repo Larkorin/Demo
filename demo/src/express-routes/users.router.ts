a99a5d625b3456bea19b1c22cd1f184cfadee4c2
Unxpected155 <gabripb2004@gmail.com>
2023-06-06 15:39:29 -0600

    Commit getUsers

--- demo/src/express-routes/users.router.ts
+++ demo/src/express-routes/users.router.ts
@@ -9,6 +9,15 @@ usersRouter.use(express.json());
 
 // GET
 
+usersRouter.get("/", async (_req: Request, res: Response) => {
+  try {
+     const users = (await collections.users?.find({}).toArray()) as unknown as User[];
+
+      res.status(200).send(users);
+  } catch (error:any) {
+      res.status(500).send(error.message);
+  }
+});
 
 // POST
 usersRouter.post("/", async (req: Request, res: Response) => {
